3dba6293ebb1f45dc90d92d54f2a29627805e9a6
ivykw <ivyw219@gmail.com>
2022-03-03 15:20:39 -0600

    one rook solution done

--- src/solvers.js
+++ src/solvers.js
@@ -16,12 +16,75 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+/*
+I: number of rooks on nxn chessboard
+O: board object
+C:
+E: n is negative/0
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+~~ High Level Strategy ~~
+Toggle piece on space, check if there's a conflict, have a piece placed boolean to exit recursive
+
+~~ Pseudocode ~~
+// create board
+// set rows var
+// create recursive function
+  // piece placed var = false
+  // iterate over rows
+    // toggle piece
+    // piece placed = true
+    // if conflicting
+      // toggle piece again
+      // piece placed = false
+      // continue iterating
+    // else
+      // if recursive false  -- piecePlaced = addPiece();
+        // continue loop
+      // move along
+  //return piecePlaced
+
+*/
+  var newBoard = new Board({'n': n});
+  var rows = newBoard.rows();
+  var addPiece = function(startingRow) {
+    var startingRow = startingRow || 0;
+    var piecePlaced = false;
+    if (startingRow === rows.length - 1) {
+      for (var k = 0; k < rows.length && !piecePlaced; k++) {
+        newBoard.togglePiece(startingRow, k);
+        piecePlaced = true;
+        if (newBoard.hasRowConflictAt(startingRow) || newBoard.hasColConflictAt(k)) {
+          newBoard.togglePiece(startingRow, k);
+          piecePlaced = false;
+        }
+      }
+      return piecePlaced;
+    }
+
+    for (var i = startingRow; i < rows.length && !piecePlaced; i++) {
+      for (var j = 0; j < rows.length && !piecePlaced; j++) {
+        newBoard.togglePiece(i, j);
+        piecePlaced = true;
+        if (newBoard.hasRowConflictAt(i) || newBoard.hasColConflictAt(j)) {
+          newBoard.togglePiece(i, j);
+          piecePlaced = false;
+        } else {
+          piecePlaced = addPiece(i + 1);
+        }
+      }
+    }
+    return piecePlaced;
+  }
+  addPiece();
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(rows));
+  return rows;
 };
 
+/*
+var solutionBoard = new Board(findNRooksSolution(n));
+findNRooksSolution === [1]
+[[1]]
+*/
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
